a8b293f60f5829bfd796ebeae98aa302ac779b99
Laksh Kundnani <kundnanl@sheridancollege.ca>
2023-09-07 11:24:45 -0400

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,11 +1,11 @@
 export const geoApiOptions = {
     method: "GET",
     headers: {
-      "X-RapidAPI-Key": "0a55960b4cmshb730e2d816c1d59p174a57jsn542cd5a7fb2a",
+      "X-RapidAPI-Key": "API_TOKEN", //replace with you rapid  api token
       "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
     },
   };
   export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
   
   export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
-  export const WEATHER_API_KEY = "7a4cc5b96173139fb03fe778211cc321";
+  export const WEATHER_API_KEY = "API_TOKEN"; //replace with you api token
